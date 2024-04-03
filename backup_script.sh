a89b93d1bdf16c44acc17c9377b338120cdeb072
keithhubner <keith@hubner.co.uk>
2024-04-03 11:48:22 +0100

    Added debug

--- backup_script.sh
+++ backup_script.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+set -e
+
+# Define a function to handle errors
+handle_error() {
+    echo "An error occurred in the script at line $1"
+    # You can perform any cleanup or logging actions here
+}
+
+# Set up error handling with trap
+trap 'handle_error $LINENO' ERR
 
 # Create the backup directory if it doesn't exist
 echo "Creating backup directory..."
@@ -19,16 +29,18 @@ echo "[$LOG_TIMESTAMP] Starting backup..." >> $APP_DIR/logs/event.log
 BACKUP_FILE="$APP_DIR/backups/$DB_NAME-$TIMESTAMP.sql"
 echo "Backup file: $BACKUP_FILE"
 
+
 # Perform the backup using mysqldump
-mariadb-dump --ssl -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > $BACKUP_FILE 2>> "$APP_DIR/logs/error.log"
+mariadb-dump --ssl -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > $BACKUP_FILE
+
 
 # Optionally compress the backup file (uncomment the line below if you want to compress)
 # gzip "$APP_DIR/$DB_NAME-$TIMESTAMP.sql"
 
 # Check if mysqldump encountered any errors
-if [ $? -ne 0 ]; then
-    echo "[$LOG_TIMESTAMP] Error: mysqldump encountered an error. See $APP_DIR/logs/error.log for details." >> $APP_DIR/logs/event.log
-    rm -rf $BACKUP_FILE
-else
-    echo "[$LOG_TIMESTAMP] Backup completed successfully: $BACKUP_FILE" >> $APP_DIR/logs/event.log
-fi
+# if [ $? -ne 0 ]; then
+#     echo "[$LOG_TIMESTAMP] Error: mysqldump encountered an error. See $APP_DIR/logs/error.log for details." >> $APP_DIR/logs/event.log
+#     rm -rf $BACKUP_FILE
+# else
+#     echo "[$LOG_TIMESTAMP] Backup completed successfully: $BACKUP_FILE" >> $APP_DIR/logs/event.log
+# fi
