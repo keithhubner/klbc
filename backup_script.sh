bb9f2d2ccbcfd425f7061dd9bd5e083f5e20576b
keithhubner <keith@hubner.co.uk>
2024-04-03 11:27:03 +0100

    Logging messages

--- backup_script.sh
+++ backup_script.sh
@@ -8,11 +8,16 @@ mkdir -p "$APP_DIR/logs"
 
 echo "Backup directory created successfully."
 
+echo "Starting backup..."
+
 # Generate a timestamp for the backup file
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 LOG_TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
+echo "[$LOG_TIMESTAMP] Starting backup..." >> $APP_DIR/logs/event.log
+
 BACKUP_FILE="$APP_DIR/backups/$DB_NAME-$TIMESTAMP.sql"
+echo "Backup file: $BACKUP_FILE"
 
 # Perform the backup using mysqldump
 mariadb-dump --ssl -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > $BACKUP_FILE 2>> "$APP_DIR/logs/error.log"
