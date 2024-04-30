a939224b55fbe13086e70ca5fb364fdaaa1ab83a
keithhubner <keith@hubner.co.uk>
2024-04-30 11:45:13 +0100

    Added Cleanup

--- backup_script.sh
+++ backup_script.sh
@@ -41,3 +41,36 @@ echo "Running S3 Backup...."
 s3cmd --host=${AWS_HOST}  --host-bucket=s3://${BUCKET} put --acl-${PUB_PRIV} ${BACKUP_FILE} s3://${S3_PATH}
 
 # Adding a change to test
+
+echo "Running Cleanup...."  
+
+# Cleanup   
+
+#!/bin/bash
+
+# Specify the number of days
+OLDER_THAN_DAYS=30
+
+# Current date in seconds
+CURRENT_DATE=$(date +%s)
+
+# List all files in the S3 bucket with their timestamps
+s3cmd ls s3://${BUCKET}/ --recursive | while read -r line; do
+  # Extract the date and file path
+  FILE_DATE=$(echo $line | awk '{print $1}')
+  FILE_TIME=$(echo $line | awk '{print $2}')
+  FILE_NAME=$(echo $line | awk '{print $4}')
+
+  # Combine date and time, then convert to seconds since epoch
+  FILE_DATETIME="$FILE_DATE $FILE_TIME"
+  FILE_DATE_SECONDS=$(date -d"$FILE_DATETIME" +%s)
+
+  # Calculate the file's age in days
+  AGE=$(( ($CURRENT_DATE - $FILE_DATE_SECONDS) / 86400 ))
+
+  # If the file is older than the specified number of days, delete it
+  if [ $AGE -gt $OLDER_THAN_DAYS ]; then
+    echo "Deleting $FILE_NAME which is $AGE days old."
+    s3cmd del $FILE_NAME
+  fi
+done
