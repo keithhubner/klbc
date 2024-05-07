a911ccc8b552a3cad85b67ed8a8af5907a42fb32
keithhubner <keith@hubner.co.uk>
2024-05-07 10:02:21 +0100

    backup filename

--- backup_script.sh
+++ backup_script.sh
@@ -48,7 +48,7 @@ function run_backup() {
 
 function run_s3_backup() {
     echo "Running S3 Backup...."
-    s3cmd --host=${AWS_HOST}  --host-bucket=s3://${BUCKET} put --acl-${PUB_PRIV} ${BACKUP_FILE} s3://${S3_PATH}
+    s3cmd --host=${AWS_HOST}  --host-bucket=s3://${BUCKET} put --acl-${PUB_PRIV} $BACKUP_FILE s3://${S3_PATH}
 }
 
 function cleanup() {
